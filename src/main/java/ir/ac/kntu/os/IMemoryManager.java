ff7e3c0b85f1ef8c32711557a2139afc08a40efd
mohammad shafiee <shafiee.mohamin@gmail.com>
2021-01-23 13:30:15 +0330

    change convention of deallocating function

--- src/main/java/ir/ac/kntu/os/IMemoryManager.java
+++ src/main/java/ir/ac/kntu/os/IMemoryManager.java
@@ -1,11 +1,14 @@
 package ir.ac.kntu.os;
 
 import ir.ac.kntu.errors.MaximumAllocationError;
+import ir.ac.kntu.errors.NoneDellocatingBlockError;
 
 public interface IMemoryManager {
     int MAX_MEMORY_SIZE = 8192;
     int MIN_MEMORY_SZIE = 2048;
-    void deAllocation(int pid, long address);
+    int MIN_BLOCK_SIZE = 32;
+    int MAX_BLOCK_SIZE = 1024;
+    void deAllocation(int pid, long address) throws NoneDellocatingBlockError;
     long allocation(int pid, int size);
     void setMemorySize(int size) throws MaximumAllocationError;
     int getMemorySize();
