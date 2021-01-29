4735c8b7f533ddaebd71198598694d46ed8af980
mohammad shafiee <shafiee.mohamin@gmail.com>
2021-01-29 19:48:29 +0330

    Added finish lock to project

--- src/main/java/ir/ac/kntu/os/Locker.java
+++ src/main/java/ir/ac/kntu/os/Locker.java
@@ -5,34 +5,52 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 public class Locker {
     private ReentrantReadWriteLock occupyListLlLock;
     private ReentrantReadWriteLock treeLock;
+    private ReentrantReadWriteLock finishLock;
     private static Locker instance;
 
     private Locker() {
         this.occupyListLlLock = new ReentrantReadWriteLock();
         this.treeLock = new ReentrantReadWriteLock();
+        this.finishLock = new ReentrantReadWriteLock();
     }
 
     public static Locker getLockerInstance() {
-        if (Locker.instance == null)
+        if (Locker.instance == null) {
             Locker.instance = new Locker();
-
+        }
         return Locker.instance;
     }
 
-    public void readUnlockList() {
-        this.occupyListLlLock.readLock().lock();
+    public void readFinishLock() {
+        this.finishLock.readLock().lock();
     }
 
-    public void readLockList() {
+    public void readFinishUnlock() {
+        this.finishLock.readLock().unlock();
+    }
+
+    public void writeFinishLock() {
+        this.finishLock.writeLock().lock();
+    }
+
+    public void writeFinishUnlock() {
+        this.finishLock.writeLock().unlock();
+    }
+
+    public void readUnlockList() {
         this.occupyListLlLock.readLock().unlock();
     }
 
+    public void readLockList() {
+        this.occupyListLlLock.readLock().lock();
+    }
+
     public void writeLockList() {
-        this.treeLock.writeLock().lock();
+        this.occupyListLlLock.writeLock().lock();
     }
 
     public void writeUnlockList() {
-        this.treeLock.writeLock().unlock();
+        this.occupyListLlLock.writeLock().unlock();
     }
 
     public void readLockTree() {
