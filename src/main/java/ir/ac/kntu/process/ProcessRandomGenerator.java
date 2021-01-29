34173649387f1bbd93ef9a7de4c365e3b0a229c7
mohammad shafiee <shafiee.mohamin@gmail.com>
2021-01-29 19:44:00 +0330

    Class for generating the random cases

---
+++ src/main/java/ir/ac/kntu/process/ProcessRandomGenerator.java
@@ -0,0 +1,56 @@
+package ir.ac.kntu.process;
+
+import java.util.ArrayList;
+
+public class ProcessRandomGenerator {
+
+    public static int randomRequestCount(int start, int last) {
+        return (int) ProcessRandomGenerator.randomRange(start, last);
+    }
+
+    public static int makeProcessWeightedProbAllocatingRequest() {
+        ArrayList<Integer> weightedProp = new ArrayList<>();
+        weightedProp.addAll(makeRandomForSpecificRange(10, 0, 32));
+        weightedProp.addAll(makeRandomForSpecificRange(6, 32, 64));
+        weightedProp.addAll(makeRandomForSpecificRange(5, 64, 128));
+        weightedProp.addAll(makeRandomForSpecificRange(5, 128, 256));
+        weightedProp.addAll(makeRandomForSpecificRange(5, 256, 512));
+        weightedProp.addAll(makeRandomForSpecificRange(2, 256, 1024));
+        try {
+            return weightedProp.get((int) randomRange(0, weightedProp.size()));
+        } catch (Exception ex) {
+            return weightedProp.get(0);
+        }
+    }
+
+    private static ArrayList<Integer> makeRandomForSpecificRange(int range, int start, int end) {
+        ArrayList<Integer> rands = new ArrayList<>();
+        for (int i = 0; i < range; i++) {
+            int rand = (int) randomRange(start, end);
+            if (rands.contains(rand)) {
+                i -= 1;
+                continue;
+            }
+            rands.add(rand);
+        }
+        return rands;
+    }
+
+    public static long randomSleepTime(int start, int last) {
+        double randNum = ProcessRandomGenerator.randomRange(start, last);
+        return (long) (randNum * 1000);
+    }
+
+    public static double randomRange(int start, int last) {
+        return (Math.random() * (double) (last - start + 1)) + start;
+    }
+
+    public static Jobs randomJob() {
+        int op = (int) (Math.random() * 2);
+        for (Jobs jobs : Jobs.values()) {
+            if (jobs.ordinal() == op)
+                return jobs;
+        }
+        return Jobs.DEALLOCATE;
+    }
+}
