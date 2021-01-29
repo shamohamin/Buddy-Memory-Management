873a8c4b7fea8f86a3537c26b087bc4f80f92575
mohammad shafiee <shafiee.mohamin@gmail.com>
2021-01-29 19:41:52 +0330

    interface for keeping track of process configs

--- src/main/java/ir/ac/kntu/process/IProcess.java
+++ src/main/java/ir/ac/kntu/process/IProcess.java
@@ -1,6 +1,10 @@
 package ir.ac.kntu.process;
 
-public interface IProcess {
-    void deAllocation(int pid, long address);
-    long allocation(int pid, int size);
+public interface IProcess extends Runnable {
+    int START_SLEEP_TIME = 3;
+    int END_SLEEP_TIME = 7;
+    int MAX_THREAD_WORKER = 1;
+    int MAX_REQUEST_SIZE = 1024;
+    void deAllocation(long address);
+    long allocation(int size);
 }
